511a0c0dd5fb5f367b96544060d715e0e591f5cd
Zachary Truong <zacharytruong@gmail.com>
2024-07-24 19:08:04 -0400

    add static layout of the directory

--- components/CharacterGrid.tsx
+++ components/CharacterGrid.tsx
@@ -1,14 +1,73 @@
 'use client';
 
-import useGetPeople from '@/hooks/getPeople';
+import getNameAbbreviation from '@/lib/getNameAbbreviation';
+import { CiCircleQuestion } from 'react-icons/ci';
 
-export default function CharacterGrid({ id }: { id?: string }) {
-  const peopleQuery = useGetPeople(id);
-  if (!id) {
-    return 'No results.';
-  }
-  if (peopleQuery.data) {
-    console.log(peopleQuery.data.data);
-  }
-  return <div>Grid</div>;
+interface Character {
+  id?: string;
+  name?: string;
+  homeWorld?: string;
+  specie?: string;
+}
+
+export default function CharacterGrid({
+  // characters = [
+  //   {
+  //     id: '1',
+  //     name: 'Andrew Addison',
+  //     homeWorld: 'Earth',
+  //     specie: 'Human',
+  //   },
+  //   {},
+  // ],
+  characters,
+}: {
+  characters?: Character[] | null;
+}) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-4 p-4 glass rounded-xl">
+      {!characters && (
+        <div className="text-white">You can start searching anytime!</div>
+      )}
+      {characters &&
+        characters.map((character: Character) => (
+          <div
+            key={character.id}
+            className="card bg-base-100 shadow-xl transform transition-transform duration-300 hover:scale-105 gap-8"
+          >
+            <figure className="px-10 pt-10">
+              <div className="avatar">
+                <div className="avatar placeholder">
+                  <div className="bg-primary text-neutral-content w-24 rounded-full">
+                    <span className="text-3xl">
+                      {getNameAbbreviation(character.name) || (
+                        <CiCircleQuestion />
+                      )}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title p-4">
+                {character.name || 'No name found!'}
+              </h2>
+              <p>
+                Home-world:{' '}
+                <span>
+                  {character.homeWorld ||
+                    `This character's home-world is a mystery!`}
+                </span>
+              </p>
+              <p>
+                Species:{' '}
+                <span>
+                  {character.specie || `This character's specie is a mystery!`}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
