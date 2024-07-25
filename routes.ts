f8b9d8cb381d01340dde72a876c7c85c905201e4
Zachary Truong <zacharytruong@gmail.com>
2024-07-25 11:32:26 -0400

    clean up routes

--- routes.ts
+++ routes.ts
@@ -1,14 +1,3 @@
 export const getAPIRoute = (field?: string) => {
   return `https://swapi.dev/api/${field}`;
 };
-
-const apiRoute = 'https://swapi.dev/api';
-
-export const APIRoutes = {
-  characters: (id?: string) =>
-    !id ? `${apiRoute}/people` : `${apiRoute}/people/${id}`,
-  planets: (id?: string) =>
-    !id ? `${apiRoute}/planets` : `${apiRoute}/planets/${id}`,
-  species: (id?: string) =>
-    !id ? `${apiRoute}/species` : `${apiRoute}/species/${id}`,
-};
