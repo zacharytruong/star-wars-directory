4a7bdc342b37150e24bb4cc672e336779b0eac2d
Zachary Truong <zacharytruong@gmail.com>
2024-07-25 16:15:27 -0400

    refactor useForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,15 @@
 'use client';
 
-import { SubmitHandler, useForm } from 'react-hook-form';
-import {
-  availableSearchFields,
-  ResponseData,
-  SearchFormData,
-} from '@/customTypes';
+import { availableSearchFields, ResponseData } from '@/customTypes';
 import { useState } from 'react';
-import { DataCardGrid, ErrorFetching, SearchForm } from '@/components';
-import { getData } from '@/lib';
+import { DataCardGrid, SearchForm } from '@/components';
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<SearchFormData>({
-    mode: 'onSubmit',
-  });
-
   const [dataCards, setDataCards] = useState<ResponseData[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [hasError, setHasError] = useState<boolean>(false);
   // Search mode to control the display of data cards.
   const [searchMode, setSearchMode] = useState<availableSearchFields>();
 
-  const onSubmit: SubmitHandler<SearchFormData> = async (formData) => {
-    setIsLoading(true);
-    try {
-      const data = await getData(formData.searchField, formData.searchValue);
-      setDataCards(data);
-      setSearchMode(formData.searchField);
-    } catch (error) {
-      setHasError(true);
-      console.error(error);
-    }
-    reset();
-    setIsLoading(false);
-  };
   return (
     <main>
       <h1 className="text-center p-4 text-white text-4xl font-bold">
@@ -49,17 +20,16 @@ export default function Home() {
         species.
       </p>
       <SearchForm
-        onSubmit={handleSubmit(onSubmit)}
-        disabled={isLoading}
-        register={register}
-        errors={errors}
+        isLoading={isLoading}
+        setDataCards={setDataCards}
+        setIsLoading={setIsLoading}
+        setSearchMode={setSearchMode}
       />
       <DataCardGrid
         dataCards={dataCards}
         isLoading={isLoading}
         searchMode={searchMode}
       />
-      {hasError && <ErrorFetching />}
     </main>
   );
 }
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,26 +1,58 @@
-import { FieldErrors, UseFormRegister } from 'react-hook-form';
-import { SearchFormData } from '@/customTypes';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  availableSearchFields,
+  ResponseData,
+  SearchFormData,
+} from '@/customTypes';
 import { FaSearchengin } from 'react-icons/fa';
+import { getData } from '@/lib';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { ErrorFetching } from '@/components/index';
 
 export default function SearchForm({
-  onSubmit,
-  disabled,
-  register,
-  errors,
+  isLoading,
+  setDataCards,
+  setIsLoading,
+  setSearchMode,
 }: {
-  onSubmit: () => void;
-  disabled: boolean;
-  register: UseFormRegister<SearchFormData>;
-  errors: FieldErrors<SearchFormData>;
+  isLoading: boolean;
+  setDataCards: Dispatch<SetStateAction<ResponseData[] | null>>;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+  setSearchMode: Dispatch<SetStateAction<availableSearchFields | undefined>>;
 }) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<SearchFormData>({
+    mode: 'onSubmit',
+  });
+
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const onSubmit: SubmitHandler<SearchFormData> = async (formData) => {
+    setIsLoading(true);
+    try {
+      const data = await getData(formData.searchField, formData.searchValue);
+      setDataCards(data);
+      setSearchMode(formData.searchField);
+    } catch (error) {
+      setHasError(true);
+      console.error(error);
+    }
+    reset();
+    setIsLoading(false);
+  };
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col md:flex-row gap-4 m-4 p-4 glass rounded-xl">
         <div>
           <select
             className="select select-bordered w-full md:w-auto"
             defaultValue=""
-            disabled={disabled}
+            disabled={isLoading}
             {...register('searchField', {
               required: true,
             })}
@@ -40,7 +72,7 @@ export default function SearchForm({
           <label className="input input-bordered flex items-center gap-4 w-full">
             <input
               type="text"
-              disabled={disabled}
+              disabled={isLoading}
               className="grow"
               placeholder="Enter your search here"
               {...register('searchValue', {
@@ -56,12 +88,13 @@ export default function SearchForm({
           <button
             type="submit"
             className="btn btn-primary w-full tracking-[0.75em]"
-            disabled={disabled}
+            disabled={isLoading}
           >
             SEARCH <FaSearchengin />
           </button>
         </div>
       </div>
+      {hasError && <ErrorFetching />}
     </form>
   );
 }
